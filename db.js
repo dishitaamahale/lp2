eb1c4b854164d485fa1a150a9a5d0e6cbf6a8b5a
dishitaamahale <60579142+dishitaamahale@users.noreply.github.com>
2022-05-12 09:41:43 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,17 @@
+const c = require("config")
+const config = require("config")
+const mongoose = require("mongoose")
+const mongouri  = config.get("MONGO_URI")
+const  connectDB = async ()=>{
+        try {
+            await mongoose.connect(mongouri)
+            console.log("Database connected");
+        } catch (error) {
+            console.log("Error message: " ,  error);
+        }
+}
+
+
+module.exports =  connectDB;
+
+
